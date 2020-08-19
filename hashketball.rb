9cdc2290f1ae56f43f6a4c5759f6ffbee9cc36e4
Payton Gray <paytongray18@gmail.com>
2020-08-19 17:26:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,10 @@
-# Write your code here!
+def game_hash(data)
+  hash{
+    :home
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black","White"],
+      :players => [{
+        :player_name => "Alan Anderson",
+        :number => "0",
+        
+  }
