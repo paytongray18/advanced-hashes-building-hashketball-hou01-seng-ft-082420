fab2fd501cfed9046bffd9077e1d2e262525115e
Payton Gray <paytongray18@gmail.com>
2020-08-19 17:55:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -90,6 +90,16 @@ def game_hash(data)
         :blocks => "5",
         :slam_dunks => "5"
       },
+      { :player_name => "Ben Gordon",
+        :number => "8",
+        :shoe => "15",
+        :points => "33",
+        :rebounds => "3",
+        :assists => "2",
+        :steals => "1",
+        :blocks => "1",
+        :slam_dunks => "0"
+      },
       { 
       }]
     }
