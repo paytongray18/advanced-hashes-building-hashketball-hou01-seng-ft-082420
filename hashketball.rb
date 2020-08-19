b200da54a0a261297dbdd3645f27c9a77082295e
Payton Gray <paytongray18@gmail.com>
2020-08-19 17:51:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -60,6 +60,16 @@ def game_hash(data)
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise","Purple"]
       :players => [{
-        
+        :player_name => "Jeff Adrien",
+        :number => "4",
+        :shoe => "18",
+        :points => "10",
+        :rebounds => "1",
+        :assists => "1",
+        :steals => "2",
+        :blocks => "7",
+        :slam_dunks => "2"
+      }
+      { 
       }]
     }
