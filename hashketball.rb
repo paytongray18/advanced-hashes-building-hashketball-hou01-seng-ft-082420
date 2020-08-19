41894995ab7a879aa851896aec30fbe982890b2e
Payton Gray <paytongray18@gmail.com>
2020-08-19 17:38:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -24,5 +24,15 @@ def game_hash(data)
         :blocks => "12",
         :slam_dunks => "7"
         }
+        {:player_name => "Brook Lopez",
+        :number => "11",
+        :shoe => "17",
+        :points => "17",
+        :rebounds => "19",
+        :assists => "10",
+        :steals => "3",
+        :blocks => "1",
+        :slam_dunks => "15"
+        }
         
   
